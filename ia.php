e5e491e86f1f930e41c6999bc500bd6aebdfdd57
André Reis <101909552+andrereissz@users.noreply.github.com>
2024-06-17 20:43:12 -0300

    fixes code standards

--- ia.php
+++ ia.php
@@ -2,7 +2,7 @@
 
 include('apikey.php');
 
-//$questao = $_POST['prompt'];
+$questao = $_POST['input'];
 
 // A URL da API OpenAI
 $url = 'https://api.openai.com/v1/chat/completions';
@@ -40,18 +40,7 @@ $mensagem = "
     para cada restrição, existe uma relação atrelada. No campo relações, deve ser colocado na ordem em que as restrições se apresentam, de modo que caso a restrição seja do tipo <=, você deve usar o valor '<=' no campo. Caso a restrição seja do tipo =, você deve usar o valor '=' no campo e caso a restrição seja do tipo >=, você deve usar o valor '>=' no campo.
     Sua resposta deve conter apenas o JSON, conforme o padrão apresentado, com os valores das equações do problema sem nenhuma explicação adicional.
 
-    Problema a ser resolvido seguindo o modelo:";
-
-$prompt = "
-    Uma pessoa tem disponível carne e ovos para se alimentar.
-    Cada unidade de carne contém 4 unidades de vitaminas e 6 unidades de
-    proteínas, a um custo de 3 unidades monetárias por unidade.
-    Cada unidade de ovo contém 8 unidades de vitaminas e 6 unidades de
-    proteínas, a um custo de 2,5 unidades monetárias por unidade.
-    Qual o modelo matemático que descreve a quantidade diária de carne e
-    ovos que deve ser consumida para suprir as necessidades de vitaminas e
-    proteínas com o menor custo possível? Limite mínimo diário de ingestão de vitaminas é de 32 assim como o limite diário de ingestão de proteínas é de 36 
-";
+    Problema a ser resolvido seguindo o modelo, retorne a resposta sem explicações:";
 
 // O corpo da requisição
 $data = [
@@ -60,15 +49,13 @@ $data = [
         [
             'role' => 'user',
             'content' => '
-                ' . $mensagem . $prompt . '
+                ' . $mensagem . $questao . '
             '
         ],
     ],
     'max_tokens' => 150,
 ];
 
-
-var_dump($data);
 // Inicialize o cURL
 $ch = curl_init($url);
 
@@ -90,7 +77,7 @@ $responseData = json_decode($response, true);
 // Imprima a resposta
 if (isset($responseData['choices'][0]['message']['content'])) {
     $reply = $responseData['choices'][0]['message']['content'];
-    echo 'Resposta do ChatGPT: ' . $reply;
+    echo $reply;
 } else {
     echo 'Erro ao obter a resposta do ChatGPT';
 }
