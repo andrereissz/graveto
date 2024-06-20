a5f12cb21b7b100649a3e04ddc217df4219cdf01
André Reis <101909552+andrereissz@users.noreply.github.com>
2024-06-20 12:43:25 -0300

    final

--- ia.php
+++ ia.php
@@ -76,6 +76,8 @@ $responseData = json_decode($response, true);
 $json = $responseData["choices"][0]["message"]["content"];
 $jsonData = json_decode($json, true);
 
+var_dump($response);
+
 $problema = $jsonData['problema'];
 $objetivo = $jsonData['objetivo'];
 $restricoes = $jsonData['restricoes'];
@@ -108,7 +110,7 @@ function like($needle, $haystack)
 
 <body>
     <div class="divzona d-flex flex-column min-vh-100 min-vw-100" style="background-color: #C6FFCB;">
-    <a href="index.php" class="btn position-absolute rounded-circle m-2" style="width: 100px; height: 100px; background-image: url('views/components/btn.png'); background-size: 100% 100%; scale: -1"></a>
+        <a href="index.php" class="btn position-absolute rounded-circle m-2" style="width: 100px; height: 100px; background-image: url('views/components/btn.png'); background-size: 100% 100%; scale: -1"></a>
         <div class="container d-flex flex-grow-1 justify-content-center align-items-center">
             <div class="card text-center border-dark">
                 <div class="card-header" style="font-family: Krona One;">
